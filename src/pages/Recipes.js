fd2025f4f2b175b922c770bf7d00f5d93ec7ba5a
jigspatel_it <jigspatel.04@gmail.com>
2020-01-08 16:36:49 +0530

    created recipe,recipelist,search component and worked on searched component

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import RecipeList from '../Components/RecipeList';
+import Search from '../Components/Search';
+import { recipeData } from '../data/tempList';
 
 class Recipes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    recipes: recipeData,
+    search: ''
+  };
+
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
-    return <h4>hello from recipes page</h4>;
+    return (
+      <>
+        <Search search={this.state.search} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+        <RecipeList recipes={this.state.recipes} />
+      </>
+    );
   }
 }
 
